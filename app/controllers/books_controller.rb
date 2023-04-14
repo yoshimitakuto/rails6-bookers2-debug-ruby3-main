490eb75b729ba551864033da7cc5f90245903e86
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-14 14:26:10 +0000

    [Finish]応用課題2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  has_one_attached :profile_image
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
